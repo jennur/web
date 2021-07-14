622f32a08537658495e39e781b0b0571d6470676
Elizaveta Ragozina <svillyfly@mail.ru>
2021-07-14 09:26:12 +0200

    Grouping in Shared With Others (#17)

--- packages/web-app-files/src/views/SharedWithOthers.vue
+++ packages/web-app-files/src/views/SharedWithOthers.vue
@@ -19,18 +19,17 @@
         id="files-shared-with-others-table"
         v-model="selected"
         class="files-table"
-        :class="{ 'files-table-squashed': !sidebarClosed }"
+        :class="{ 'files-table-squashed': isSidebarOpen }"
         :are-thumbnails-displayed="displayThumbnails"
         :resources="activeFiles"
         :target-route="targetRoute"
+        :highlighted="highlightedFile ? highlightedFile.id : null"
         :header-position="headerPosition"
-        @showDetails="$_mountSideBar_showDefaultPanel"
+        :grouping-settings="groupingSettings"
+        @showDetails="$_mountSideBar_showDetails"
         @fileClick="$_fileActions_triggerDefaultAction"
         @rowMounted="rowMounted"
       >
-        <template #contextMenu="{ resource }">
-          <context-actions :item="resource" />
-        </template>
         <template #footer>
           <pagination />
           <list-info
@@ -50,7 +49,6 @@ import { mapGetters, mapState, mapActions, mapMutations } from 'vuex'
 
 import { aggregateResourceShares } from '../helpers/resources'
 import FileActions from '../mixins/fileActions'
-import MixinFilesListFilter from '../mixins/filesListFilter'
 import MixinFilesListPositioning from '../mixins/filesListPositioning'
 import MixinResources from '../mixins/resources'
 import MixinFilesListPagination from '../mixins/filesListPagination'
@@ -63,20 +61,18 @@ import ListLoader from '../components/FilesList/ListLoader.vue'
 import NoContentMessage from '../components/FilesList/NoContentMessage.vue'
 import ListInfo from '../components/FilesList/ListInfo.vue'
 import Pagination from '../components/FilesList/Pagination.vue'
-import ContextActions from '../components/FilesList/ContextActions.vue'
 
 const visibilityObserver = new VisibilityObserver()
 
 export default {
-  components: { ListLoader, NoContentMessage, ListInfo, Pagination, ContextActions },
+  components: { ListLoader, NoContentMessage, ListInfo, Pagination },
 
   mixins: [
     FileActions,
     MixinFilesListPositioning,
     MixinResources,
     MixinFilesListPagination,
-    MixinMountSideBar,
-    MixinFilesListFilter
+    MixinMountSideBar
   ],
 
   data: () => ({
@@ -87,6 +83,7 @@ export default {
     ...mapState(['app']),
     ...mapState('Files', ['files']),
     ...mapGetters('Files', [
+      'davProperties',
       'highlightedFile',
       'activeFiles',
       'selectedFiles',
@@ -94,19 +91,48 @@ export default {
       'totalFilesCount'
     ]),
     ...mapGetters(['isOcis', 'configuration', 'getToken', 'user']),
-    ...mapState('Files/sidebar', { sidebarClosed: 'closed' }),
+
+    groupingSettings() {
+      return {
+        groupingBy: 'Shared date/on',
+        showGroupingOptions: true,
+        groupingFunctions: {
+          'Name alphabetically': function(row) {
+            if (!isNaN(row.name.charAt(0))) return '#'
+            if (row.name.charAt(0) === '.') return row.name.charAt(1).toLowerCase()
+            return row.name.charAt(0).toLowerCase()
+          },
+          'Shared date/on': function(row) {
+            const interval1 = new Date()
+            interval1.setDate(interval1.getDate() - 7)
+            const interval2 = new Date()
+            interval2.setDate(interval2.getDate() - 30)
+            if (row.sdate > interval1.getTime()) {
+              return 'Recent'
+            } else if (row.sdate > interval2.getTime()) {
+              return 'This Month'
+            } else return 'Older'
+          }
+        }
+      }
+    },
 
     selected: {
       get() {
         return this.selectedFiles
       },
       set(resources) {
-        this.SET_FILE_SELECTION(resources)
+        this.SELECT_RESOURCES(resources)
       }
     },
 
     isEmpty() {
+      console.log(this.activeFiles)
       return this.activeFiles.length < 1
+    },
+
+    isSidebarOpen() {
+      return this.highlightedFile !== null
     },
 
     uploadProgressVisible() {
@@ -150,7 +176,7 @@ export default {
     ...mapActions('Files', ['loadIndicators', 'loadPreview', 'loadAvatars']),
     ...mapMutations('Files', [
       'LOAD_FILES',
-      'SET_FILE_SELECTION',
+      'SELECT_RESOURCES',
       'CLEAR_CURRENT_FILES_LIST',
       'UPDATE_RESOURCE'
     ]),
