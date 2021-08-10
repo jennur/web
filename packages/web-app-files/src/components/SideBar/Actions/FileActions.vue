58829ac1eba38fbed3df5cf31351247bd0ea51d1
elizavetaRa <svillyfly@mail.ru>
2021-08-10 10:35:52 +0200

    get app links

--- packages/web-app-files/src/components/SideBar/Actions/FileActions.vue
+++ packages/web-app-files/src/components/SideBar/Actions/FileActions.vue
@@ -1,24 +1,43 @@
 <template>
-  <ul id="oc-files-actions-sidebar" class="uk-list oc-mt-s">
-    <li v-for="(action, index) in actions" :key="`action-${index}`" class="oc-py-xs">
-      <component
-        :is="action.componentType"
-        v-bind="getComponentProps(action, highlightedFile)"
-        :class="['oc-text-bold', action.class]"
-        @click.stop="action.handler(highlightedFile, action.handlerData)"
-      >
-        <oc-icon :name="action.icon" size="medium" />
-        <span class="oc-files-actions-sidebar-action-label">{{
-          action.label(highlightedFile)
-        }}</span>
-        <span
-          v-if="action.opensInNewWindow"
-          class="oc-invisible-sr"
-          v-text="$gettext('(Opens in new window)')"
-        />
-      </component>
-    </li>
-  </ul>
+  <div>
+    <ul
+      v-if="highlightedFile.extension && appActions"
+      id="oc-files-actions-sidebar"
+      class="uk-list oc-mt-s"
+    >
+      <li v-for="(action, index) in appActions" :key="`app-${index}`" class="oc-py-xs">
+        <div :class="['oc-text-bold', action.class]" @click="getLink(action)">
+          <oc-icon :name="image" size="medium" />
+          <span class="oc-files-actions-sidebar-action-label">{{ 'Open in ' + action }}</span>
+          <span
+            v-if="action.opensInNewWindow"
+            class="oc-invisible-sr"
+            v-text="$gettext('(Opens in new window)')"
+          />
+        </div>
+      </li>
+    </ul>
+    <ul id="oc-files-actions-sidebar" class="uk-list oc-mt-s">
+      <li v-for="(action, index) in actions" :key="`action-${index}`" class="oc-py-xs">
+        <component
+          :is="action.componentType"
+          v-bind="getComponentProps(action, highlightedFile)"
+          :class="['oc-text-bold', action.class]"
+          @click.stop="action.handler(highlightedFile, action.handlerData)"
+        >
+          <oc-icon :name="action.icon" size="medium" />
+          <span class="oc-files-actions-sidebar-action-label">{{
+            action.label(highlightedFile)
+          }}</span>
+          <span
+            v-if="action.opensInNewWindow"
+            class="oc-invisible-sr"
+            v-text="$gettext('(Opens in new window)')"
+          />
+        </component>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -32,6 +51,9 @@ export default {
     return $gettext('Actions')
   },
   mixins: [FileActions],
+  data: () => ({
+    appList: []
+  }),
   computed: {
     ...mapGetters('Files', ['highlightedFile', 'currentFolder']),
 
@@ -44,9 +66,85 @@ export default {
           parent: this.currentFolder
         })
       )
+    },
+    appActions() {
+      const foundApps = this.loadApps().filter(
+        app => app.extension === this.highlightedFile.extension
+      )
+      console.log('highlighted from appactions', this.highlightedFile)
+      console.log(foundApps)
+      return foundApps.length > 0 && foundApps[0].apps ? foundApps[0].apps : false
     }
   },
+
   methods: {
+    async getLink(appName) {
+      console.log(appName)
+      let data = {
+        file_id: this.highlightedFile.fileId || this.highlightedFile.id,
+        app_name: appName
+      }
+
+      const headers = new Headers()
+      headers.append('Authorization', 'Bearer ' + this.getToken)
+      headers.append('X-Requested-With', 'XMLHttpRequest')
+
+      const response = await fetch('/app/open', {
+        method: 'POST',
+        headers,
+        body: JSON.stringify(data)
+      })
+      if (!response.ok) {
+        const message = `An error has occured: ${response.status}`
+        throw new Error(message)
+      }
+
+      data = await response.json()
+      console.log('got post response', data, data['app-url'])
+      if (data && data['app-url']) window.open(data['app-url'], '_blank').focus()
+    },
+
+    async loadApps() {
+      let data
+
+      const response = await fetch('/app/list', {
+        method: 'GET'
+      })
+      if (!response.ok) {
+        const message = `An error has occured: ${response.status}`
+        throw new Error(message)
+      }
+
+      data = await response.json()
+      console.log('get data', data)
+
+      /* if (!data) {
+        data = {
+          mime_types: {
+            'application/msword': {
+              app_provider_name: ['Collabora', 'Microsoft Office 365']
+            },
+            'application/octet-stream': {
+              app_provider_name: ['Collabora', 'Microsoft Office 365']
+            },
+            'application/pdf': {
+              app_provider_name: ['Microsoft Office 365']
+            },
+            'application/rtf': {
+              app_provider_name: ['Collabora', 'Microsoft Office 365']
+            },
+            'application/doc': {
+              app_provider_name: ['Collabora', 'Microsoft Office 365']
+            }
+          }
+        }} */
+
+      const appList = []
+      for (const [key, value] of Object.entries(data.mime_types)) {
+        appList.push({ extension: key.split('/')[1], apps: value.app_provider_name })
+      }
+      return appList
+    },
     getComponentProps(action, highlightedFile) {
       if (action.componentType === 'router-link' && action.route) {
         return {
