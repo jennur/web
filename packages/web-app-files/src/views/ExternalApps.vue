b80ca8fbda2c4cdedc79bd70cde608ee18c389df
elizavetaRa <svillyfly@mail.ru>
2021-08-16 16:39:03 +0200

    change iframe creation

--- packages/web-app-files/src/views/ExternalApps.vue
+++ packages/web-app-files/src/views/ExternalApps.vue
@@ -1,17 +1,16 @@
 <template>
   <main>
     <div>
-      test id {{ $route.params.file_id }} test app {{ $route.params.app }}
       <iframe v-if="app_url && method === 'GET'" class="app-iframe" :src="app_url" />
 
       <div v-if="app_url && method === 'POST' && form_parameters">
         <form :action="app_url" target="app-iframe" method="post">
           <div v-for="(item, key, index) in form_parameters" :key="index">
-            <input :name="item" :value="item" type="hidden" />
+            <input :name="key" :value="item" type="hidden" />
           </div>
         </form>
-        <span id="frameholder"></span>
       </div>
+      <span id="frameholder"></span>
     </div>
   </main>
 </template>
@@ -29,6 +28,31 @@ export default {
   }),
   computed: {
     ...mapGetters(['getToken'])
+  },
+
+  watch: {
+    form_parameters(val, oldVal) {
+      if (val && this.method === 'POST') {
+        const frameholder = document.getElementById('frameholder')
+        const frame = document.createElement('iframe')
+        frame.name = 'app-iframe'
+        frame.id = 'app-iframe'
+
+        // The title should be set for accessibility
+        frame.title = 'App Frame'
+
+        // This attribute allows true fullscreen mode in slideshow view
+        // when using PowerPoint's 'view' action.
+        frame.setAttribute('allowfullscreen', 'true')
+
+        // The sandbox attribute is needed to allow automatic redirection to the O365 sign-in page in the business user flow
+        frame.setAttribute(
+          'sandbox',
+          'allow-scripts allow-same-origin allow-forms allow-popups allow-top-navigation allow-popups-to-escape-sandbox'
+        )
+        frameholder.appendChild(frame)
+      }
+    }
   },
 
   mounted() {
@@ -64,26 +88,6 @@ export default {
       if (data.form_parameters) this.form_parameters = data.form_parameters
 
       console.log('post data', data)
-      if (this.method === 'POST') {
-        const frameholder = document.getElementById('frameholder')
-        const frame = document.createElement('iframe')
-        frame.name = 'app-iframe'
-        frame.id = 'app-iframe'
-
-        // The title should be set for accessibility
-        frame.title = 'App Frame'
-
-        // This attribute allows true fullscreen mode in slideshow view
-        // when using PowerPoint's 'view' action.
-        frame.setAttribute('allowfullscreen', 'true')
-
-        // The sandbox attribute is needed to allow automatic redirection to the O365 sign-in page in the business user flow
-        frame.setAttribute(
-          'sandbox',
-          'allow-scripts allow-same-origin allow-forms allow-popups allow-top-navigation allow-popups-to-escape-sandbox'
-        )
-        frameholder.appendChild(frame)
-      }
     } catch (error) {
       console.log(error)
     }
